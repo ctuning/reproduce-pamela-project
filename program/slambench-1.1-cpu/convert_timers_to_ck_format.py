d424bd324022157b866cb65105fd4d7d7658766b
Grigori Fursin <grigori.fursin@cTuning.org>
2015-05-12 00:16:44 +0200

    adding parsing of slambench native output to CK format (characteristics + features) - first version, will be improved

--- program/slambench-1.1-cpu/convert_timers_to_ck_format.py
+++ program/slambench-1.1-cpu/convert_timers_to_ck_format.py
@@ -9,3 +9,41 @@
 #
 # Developer: Grigori Fursin
 #
+
+import json
+
+d={}
+
+print ('Converting fine-grain timers from slambench to CK format ...')
+
+f=open('tmp-output.tmp','r')
+s=f.read()
+f.close()
+
+desc={}
+values={}
+
+l=s.split('\n')
+if len(l)>0:
+   x=l[0].split('\t')
+   ch=0
+   for q in x:
+       qx='characteristic_'+str(ch)
+       desc[qx]=q.strip()
+       values[qx]=[]
+       ch+=1
+
+for k in range(1,len(l)):
+    x=l[k].split('\t')
+    ch=0
+    for q in x:
+        qx='characteristic_'+str(ch)
+        values[qx].append(q)
+        ch+=1
+
+d['characteristics']=values
+d['characteristics_desc']=desc
+
+f=open('tmp-ck-timer.json','wt')
+f.write(json.dumps(d, indent=2, sort_keys=True)+'\n')
+f.close()
