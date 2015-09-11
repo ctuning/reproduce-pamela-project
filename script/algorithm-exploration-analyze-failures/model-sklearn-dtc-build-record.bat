50d802f8189d62442ef5ee2bb47c6ea886ab9264
Grigori Fursin <grigori.fursin@cTuning.org>
2015-09-12 00:07:17 +0200

    adding predictive modeling of some OpenCL failures (Anton and Grigori)

---
+++ script/algorithm-exploration-analyze-failures/model-sklearn-dtc-build-record.bat
@@ -0,0 +1,2 @@
+ck build model: @model-input.json @model-sklearn-dtc.json --model_data_uoa=predictive-model-of-some-slambench-opencl-failures -model_repo_uoa=reproduce-pamela-project
+
---
+++ script/algorithm-exploration-analyze-failures/model-sklearn-dtc-validate-from-recorded.bat
@@ -0,0 +1,1 @@
+ck validate model: @model-input.json @model-sklearn-dtc.json --model_data_uoa=predictive-model-of-some-slambench-opencl-failures -model_repo_uoa=reproduce-pamela-project > model-sklearn-dtc-validate.txt 
