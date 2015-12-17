25bab29f4bba0e359895b122323b4067f1460b2a
Grigori Fursin <grigori.fursin@cTuning.org>
2015-12-17 15:59:19 +0100

    fixing replay for default exploration point

--- script/algorithm-exploration/algorithm_exploration_default_replay.bat
+++ script/algorithm-exploration/algorithm_exploration_default_replay.bat
@@ -1,1 +1,1 @@
-ck replay experiment:algorithm-exploration-slambench-default --deps --dims=##characteristics#run#run_time_state#run_time_fps#exp --repetitions=3
+ck replay experiment:algorithm-exploration-slambench-default --deps --dims="*run_time_fps*
